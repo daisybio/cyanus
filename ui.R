e9e908494339ac3d426e9cb5f1dc6eaea5174fe7
Quirin Manz <qu.manz@tum.de>
2020-12-21 14:26:14 +0100

    make server part also modular

--- ui.R
+++ ui.R
@@ -1,10 +1,15 @@
+library(shiny)
+library(shinydashboard)
+library(shinyjs)
+library(shinycssloaders)
+
+
+# read all ui files
+sapply(list.files("ui", full.names = TRUE), source, environment())
+
 header <- dashboardHeader(title = "CyTOF Pipeline")
 
 sidebar <- dashboardSidebar(uiOutput("sidebar"))
-
-# read all ui files
-sapply(list.files("ui", full.names = TRUE), source)
-
 
 body <-
   dashboardBody(
