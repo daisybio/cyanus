758aaa4d2954d36a860b3c54e1536511446edfbf
Quirin Manz <qu.manz@tum.de>
2020-12-29 20:54:39 +0100

    better clustering

---
+++ server/clusterFun.R
@@ -0,0 +1,177 @@
+.get_features <- function (x, fs)
+{
+  if (is.null(fs)) {
+    fs <- rownames(x)
+  }
+  else {
+    stopifnot(is.character(fs))
+    foo <- tryCatch(
+      error = function(e)
+        e,
+      match.arg(fs,
+                c("type", "state", "none"))
+    )
+    if (!inherits(foo, "error")) {
+      fs <- foo
+      stopifnot(!is.null(marker_classes(x)))
+      fs <- rownames(x)[marker_classes(x) == fs]
+      if (length(fs) == 0)
+        stop("No features matched the specified marker class.")
+    }
+    else
+      stopifnot(fs %in% rownames(x))
+  }
+  return(fs)
+}
+
+.triangle <- function (m) 
+{
+  n <- ncol(m)
+  nm <- matrix(0, ncol = n, nrow = n)
+  fm <- m
+  nm[upper.tri(nm)] <- m[upper.tri(m)]
+  fm <- t(nm) + nm
+  diag(fm) <- diag(m)
+  nm <- fm
+  nm[upper.tri(nm)] <- NA
+  diag(nm) <- NA
+  m[lower.tri(nm)]
+}
+
+
+.plot_delta_area <- function (mc)
+{
+  library(ggplot2)
+  maxK <- length(mc)
+  v <- lapply(mc[seq_len(maxK)[-1]], function(x)
+    .triangle(x$ml))
+  h <- lapply(v, function(x) {
+    h <- graphics::hist(x, breaks = seq(0, 1, 0.01), plot = FALSE)
+    h$counts <- cumsum(h$counts) / sum(h$counts)
+    return(h)
+  })
+  areaK <- vapply(h, function(x)
+    cumsum(x$counts * 0.01)[100],
+    numeric(1))
+  deltaK <- c(areaK[1], diff(areaK) / areaK[seq_len(maxK - 2)])
+  df <- data.frame(k = seq_len(maxK)[-1], y = deltaK)
+  y_max <- ceiling(max(df$y) * 2) / 2
+  ggplot(df, aes_string(x = "k", y = "y")) + theme_classic() +
+    geom_line(color = "steelblue", lty = 2) + geom_point(size = 2.5,
+                                                         color = "navy") + coord_fixed(4) + scale_x_continuous(breaks = seq(2,
+                                                                                                                            20, 2), expand = c(0, 0.5)) + scale_y_continuous(
+                                                                                                                              limits = c(0,
+                                                                                                                                         y_max),
+                                                                                                                              expand = c(0, 0.125),
+                                                                                                                              breaks = function(x)
+                                                                                                                                seq(x[1] +
+                                                                                                                                      0.125, x[2], 0.5)
+                                                                                                                            ) + ylab("Relative change\nin area under CDF curve") +
+    theme(
+      plot.title = element_text(face = "bold"),
+      axis.text = element_text(color = "black"),
+      panel.grid.major = element_line(color = "grey",
+                                      size = 0.2)
+    )
+}
+
+
+clusterSCE <-
+  function (x,
+            method = c("flowSOM", "clusterX", "rphenoGraph"),
+            assayType,
+            features = "type",
+            xdim = 10,
+            ydim = 10,
+            maxK = 20,
+            verbose = TRUE,
+            seed = 1)
+  {
+    stopifnot(is(x, "SingleCellExperiment"))
+    stopifnot(
+      is.logical(verbose),
+      length(verbose) == 1,
+      vapply(list(xdim,
+                  ydim, maxK, seed), function(arg)
+                    is.numeric(arg) &&
+               length(arg) == 1, logical(1))
+    )
+    method <- match.arg(method)
+    features <- .get_features(x, features)
+    if (is.null(marker_classes(x))) {
+      rowData(x)$marker_class <-
+        factor(c("state", "type")[as.numeric(rownames(x) %in%
+                                               features) + 1], levels = c("type", "state", "none"))
+    }
+    rowData(x)[[sprintf("used_for_clustering_%s", method)]] <-
+      rownames(x) %in% features
+    
+    assays = c("exprs" = "Transformed", "counts" = "Raw")
+    if (method == "flowSOM") {
+      if (verbose)
+        message("o running FlowSOM clustering...")
+      fsom <- ReadInput(flowCore::flowFrame(t(assay(x, assayType))))
+      som <- FlowSOM::BuildSOM(
+        fsom,
+        colsToUse = features,
+        silent = TRUE,
+        xdim = xdim,
+        ydim = ydim
+      )
+      if (verbose)
+        message("o running ConsensusClusterPlus metaclustering...")
+      pdf(NULL)
+      mc <-
+        suppressWarnings(suppressMessages(
+          ConsensusClusterPlus::ConsensusClusterPlus(
+            t(som$map$codes),
+            maxK = maxK,
+            reps = 100,
+            distance = "euclidean",
+            seed = seed,
+            plot = NULL
+          )
+        ))
+      dev.off()
+      k <- xdim * ydim
+      mcs <- seq_len(maxK)[-1]
+      codes <- data.frame(seq_len(k), purrr::map(mc[-1], "consensusClass"))
+      codes <-
+        dplyr::mutate_all(codes, function(u)
+          factor(u, levels = sort(unique(u))))
+      colnames(codes) <- c(sprintf("som%s", k), sprintf("meta%s",
+                                                        mcs))
+      x$cluster_id <- factor(som$map$mapping[, 1])
+      metadata(x)$cluster_codes <- codes
+      metadata(x)$SOM_codes <- som$map$codes
+      metadata(x)$delta_area <- .plot_delta_area(mc)
+      metadata(x)$cluster_run$flowSOM <- list(
+        features = features,
+        assayType = assays[assayType],
+        xdim = xdim,
+        ydim = ydim,
+        maxK = maxK
+      )
+    } else if (method == "clusterX") {
+      library(cytofkit)
+      clusterx <-
+        cytofkit::ClusterX(t(assay(x, assayType)[features,]))
+      colData(x)$clusterX_id <- clusterx$cluster
+      metadata(x)$cluster_run$clusterX <- list(
+        features = features,
+        assayType = assays[assayType]
+      )
+    } else if (method == "rphenoGraph") {
+      library(cytofkit)
+      rphenograph <-
+        cytofkit::Rphenograph(t(assay(x, assayType)[features,]), maxK)
+      colData(x)$rphenoGraph_id[as.numeric(rphenograph$names)] <- rphenograph$membership
+      metadata(x)$cluster_run$rphenoGraph <- list(
+        features = features,
+        assayType = assays[assayType],
+        k = maxK
+      )
+    } else
+      stop("which method was selected?")
+    return(x)
+  }
--- server/clustering.R
+++ server/clustering.R
@@ -1,3 +1,7 @@
+### Helpers ----
+source("server/clusterFun.R", local = TRUE)
+
+### Observer ----
 observeEvent(input$startClustering, {
   updateButton(session,
                "startClustering",
@@ -5,153 +9,240 @@ observeEvent(input$startClustering, {
                disabled = TRUE)
   shinyjs::disable("clusteringInputs")
   shinyjs::disable("clusteringOutput")
+  shinyjs::disable("clusteringVisualizationSelection")
+  shinyjs::disable("sidebar")
   
-  if (input$clusteringMethod == "flowSOM") {
-    if (input$useFeatures == "Choose All")
-      features <- NULL
-    else
-      features <- isolate(input$featuresIn)
-    reactiveVals$sce <- CATALYST::cluster(reactiveVals$sce,
-                                          features,
-                                          input$xdim,
-                                          input$ydim,
-                                          input$k)
-  }
+  reactiveVals$sce <-
+    clusterSCE(
+      reactiveVals$sce,
+      input$clusteringMethod,
+      input$assayTypeIn,
+      input$featuresIn,
+      input$xdim,
+      input$ydim,
+      input$k
+    )
+  
   shinyjs::enable("clusteringInputs")
   shinyjs::enable("clusteringOutput")
-  updateButton(session,
-               "startClustering",
-               label = " Start Clustering",
-               disabled = FALSE)
+  shinyjs::enable("clusteringVisualizationSelection")
+  shinyjs::enable("sidebar")
+  updateButton(session, "startClustering", label = " Start Clustering", disabled = FALSE)
   updateButton(session, "continue", label = " Differential Expression Analysis")
   shinyjs::show("continue")
-  runjs("document.getElementById('continue').scrollIntoView();")
+})
+
+observe({
+  if (is.null(input$featuresIn))
+    disable("startClustering")
+  else
+    enable("startClustering")
+})
+
+### Renderer ----
+output$parameters <- renderUI({
+  shinydashboard::box(
+    column(
+      uiOutput("useFeaturesOut"),
+      uiOutput("featuresOut"),
+      uiOutput("assayTypeOut"),
+      width = 6
+    ),
+    column(uiOutput("k"),
+           uiOutput("xdim"),
+           uiOutput("ydim"), width = 6),
+    title = "Choose Clustering Parameters",
+    width = 10
+  )
+})
+
+output$useFeaturesOut <- renderUI({
+  selectInput(
+    "useFeaturesIn",
+    label = "Features to choose from",
+    choices = c("Marker by Class",
+                "Marker by Name")
+  )
+})
+
+output$featuresOut <- renderUI({
+  req(input$useFeaturesIn)
+  if (input$useFeaturesIn == "Marker by Class") {
+    choices <-
+      levels(SummarizedExperiment::rowData(reactiveVals$sce)$marker_class)
+  } else if (input$useFeaturesIn == "Marker by Name") {
+    choices <- rownames(reactiveVals$sce)
+    names(choices) <- sprintf("%s (%s)", choices, as.character(rowData(reactiveVals$sce)$marker_class))
+  } else
+    stop("by name or by class?")
+  shinyWidgets::pickerInput(
+    inputId = "featuresIn",
+    label = "Features to use for clustering",
+    choices = choices,
+    selected = choices[1],
+    multiple = TRUE,
+    options = list(
+      `actions-box` = TRUE,
+      `selected-text-format` = "count > 3",
+      "dropup-auto" = FALSE
+    )
+  )
+})
+
+output$assayTypeOut <- renderUI({
+  choices <- c("Transformed" = "exprs", "Raw" = "counts")
+  choices <- choices[choices %in% assayNames(reactiveVals$sce)]
+  selectizeInput(
+    "assayTypeIn",
+    "Expression Type",
+    choices = choices
+  )
 })
 
 output$k <- renderUI({
-  if (input$clusteringMethod == "clusterX")
-    return(NULL)
-  else if (input$clusteringMethod == "flowSOM") {
+  req(input$clusteringMethod != "clusterX", input$featuresIn)
+  
+  if (input$clusteringMethod == "flowSOM") {
     label = "Maximum Number of Clusters to Evaluate in the Metaclustering"
     value = 20
+    maxK = 100
   } else if (input$clusteringMethod == "rphenoGraph") {
     label = "Number of Nearest Neighbours"
     value = 30
+    maxK = length(.get_features(reactiveVals$sce, input$featuresIn)) - 1
   } else
     stop("which clustermethod was chosen?")
   sliderInput(
     inputId = "k",
     label = label,
-    value = value,
+    value = min(value, maxK),
+    min = 1,
+    max = maxK
+  )
+})
+
+output$ydim <- renderUI({
+  req(input$clusteringMethod == "flowSOM")
+  sliderInput(
+    inputId = "ydim",
+    label = "ydim of the grid size of the self-organizing map",
+    value = 10,
+    min = 2,
+    max = 100
+  )
+  
+})
+
+output$xdim <- renderUI({
+  req(input$clusteringMethod == "flowSOM")
+  sliderInput(
+    inputId = "xdim",
+    label = "xdim of the grid size of the self-organizing map",
+    value = 10,
     min = 2,
     max = 100
   )
 })
 
-output$ydim <- renderUI({
-  if (input$clusteringMethod %in% c("clusterX", "rphenoGraph"))
-    return(NULL)
-  else if (input$clusteringMethod == "flowSOM") {
-    return(
-      sliderInput(
-        inputId = "ydim",
-        label = "ydim of the grid size of the self-orginizing map",
-        value = 10,
-        min = 2,
-        max = 100
-      )
-    )
-  } else
-    stop("which clustermethod was chosen?")
+output$clusteringVisualizationSelection <- renderUI({
+  req(metadata(reactiveVals$sce)$cluster_run)
+  
+  
+  shinydashboard::box(
+    selectizeInput(
+      "clusteringRuns",
+      "Successfull Run",
+      names(metadata(reactiveVals$sce)$cluster_run)
+    ),
+    div(
+      bsButton(
+        "visualizeClustering",
+        "Visualize Clustering",
+        icon = icon("palette"),
+        style = "success"
+      ),
+      style = "float: right;"
+    ),
+    column(tableOutput("clusterRunParams"),
+           width = 5),
+    column(
+      uiOutput("selectClusterCode"),
+      withSpinner(uiOutput("clusterSizes")),
+      width = 5,
+      style = "overflow-x: scroll;"
+    ),
+    fluidRow(withSpinner(uiOutput("delta_area"))),
+    title = "Visualize Clustering Results",
+    width = 12
+  )
 })
 
-output$xdim <- renderUI({
-  if (input$clusteringMethod %in% c("clusterX", "rphenoGraph"))
-    return(NULL)
-  else if (input$clusteringMethod == "flowSOM") {
-    return(
-      sliderInput(
-        inputId = "xdim",
-        label = "xdim of the grid size of the self-orginizing map",
-        value = 10,
-        min = 2,
-        max = 100
-      )
-    )
-  } else
-    stop("which clustermethod was chosen?")
+output$clusterRunParams <- renderTable({
+  
+  runjs("document.getElementById('clusteringVisualizationSelection').scrollIntoView();")
+  runParams <-
+    metadata(reactiveVals$sce)$cluster_run[[input$clusteringRuns]]
+  runParams$features <- paste(runParams$features, collapse = ",")
+  data.frame(runParams)
+},
+caption = "Run Parameters",
+caption.placement = "top")
+
+output$selectClusterCode <- renderUI({
+  req(input$clusteringRuns == "flowSOM")
+  
+  
+  selectInput("clusterCode",
+              "Clusters",
+              rev(names(cluster_codes(reactiveVals$sce))))
 })
 
-output$useFeatures <- renderUI({
-  if (input$clusteringMethod %in% c("clusterX", "rphenoGraph"))
-    return(NULL)
-  else if (input$clusteringMethod == "flowSOM") {
-    return(selectInput(
-      "useFeatures",
-      label = "Parameters to choose from",
-      choices = c("Marker by Class",
-                  "Marker by Name",
-                  "Choose All")
-    ))
-  } else
-    stop("which clustermethod was chosen?")
-})
-
-output$featuresOut <- renderUI({
-  if (input$clusteringMethod %in% c("clusterX", "rphenoGraph"))
-    return(NULL)
-  else if (input$clusteringMethod == "flowSOM") {
-    if (is.null(input$useFeatures) ||
-        input$useFeatures == "Choose All") {
-      return(NULL)
-    } else if (input$useFeatures == "Marker by Class") {
-      choices = unique(SummarizedExperiment::rowData(reactiveVals$sce)$marker_class)
-    } else if (input$useFeatures == "Marker by Name") {
-      choices = rownames(reactiveVals$sce)
-    }
-    return(
-      selectInput(
-        inputId = "featuresIn",
-        label = "features to use for clustering",
-        choices = choices,
-        selected = choices[1],
-        multiple = TRUE
-      )
-    )
+output$clusterSizes <- renderTable({
+  if (input$clusteringRuns == "flowSOM") {
+    req(input$clusterCode)
+    res <-
+      as.data.frame(table(cluster_ids(reactiveVals$sce, input$clusterCode), useNA =
+                            "ifany"))
     
-  } else
-    stop("which clustermethod was chosen?")
-})
-
-output$dimReduction <- renderUI({
-  if (input$clusteringMethod %in% c("flowSOM", "rphenoGraph"))
-    return(NULL)
-  else if (input$clusteringMethod == "clusterX") {
-    return(
-      selectInput(
-        inputId = "dimReduction",
-        label = "Dimensionality reduction method.",
-        choices = reducedDimNames(reactiveVals$sce)
-      )
-    )
-  } else
-    stop("which clustermethod was chosen?")
-})
+  }
+  else
+    res <-
+      as.data.frame(table(colData(reactiveVals$sce)[[sprintf("%s_id", input$clusteringRuns)]], useNA =
+                            "ifany"))
+  
+  names(res) <- c("Cluster", "Size")
+  t(res)
+},
+caption = "Cluster Sizes",
+caption.placement = "top", rownames = TRUE, colnames = FALSE)
 
 output$delta_area <- renderUI({
-  if (!"delta_area" %in% names(metadata(reactiveVals$sce)))
-    return(NULL)
+  req(input$clusteringRuns == "flowSOM")
   
+  
+  runjs("document.getElementById('clusteringVisualizationSelection').scrollIntoView();")
   shinydashboard::box(renderPlot(CATALYST::delta_area(reactiveVals$sce)),
                       title = "Delta Area",
                       width = 12)
 })
 
-output$clusterSizes <- renderTable({
-  res <-
-    as.data.frame(table(cluster_ids(reactiveVals$sce, input$clusterCode)))
-  names(res) <- c("Cluster", "Size")
-  res
+output$clusteringOutput <- renderUI({
+  req(input$visualizeClustering)
+  
+  shinydashboard::box(fluidRow(
+    shinydashboard::tabBox(
+      tabPanel("Densities", withSpinner(plotOutput(
+        "clusterExprsPlot"
+      ))),
+      tabPanel("Heatmap", withSpinner(plotOutput(
+        "clusterHeatmapPlot"
+      ))),
+      title = "Cluster Visualization",
+      width = 12
+    )
+  ),
+  title = "Clustering Results",
+  width = 12)
 })
 
 output$clusterExprsPlot <- renderPlot({
@@ -162,58 +253,4 @@ output$clusterExprsPlot <- renderPlot({
 output$clusterHeatmapPlot <- renderPlot({
   CATALYST::plotFreqHeatmap(reactiveVals$sce,
                             k = input$clusterCode) # TODO: implement other parameters
-})
-
-output$clusterOverview <- renderUI({
-  div(column(
-    selectInput("clusterCode",
-                "Clusters",
-                rev(names(
-                  cluster_codes(reactiveVals$sce)
-                )),
-                selected = isolate(input$k)),
-    withSpinner(uiOutput("clusterSizes")),
-    width = 2
-  ),
-  column(withSpinner(uiOutput("delta_area")), width = 10))
-})
-
-
-output$clusteringOutput <- renderUI({
-  if (!"cluster_id" %in% names(colData(reactiveVals$sce)))
-    return(NULL)
-  
-  shinydashboard::box(
-    fluidRow(uiOutput("clusterOverview")),
-    fluidRow(
-      shinydashboard::tabBox(
-        tabPanel("Densities", withSpinner(plotOutput(
-          "clusterExprsPlot"
-        ))),
-        tabPanel("Heatmap", withSpinner(plotOutput(
-          "clusterHeatmapPlot"
-        ))),
-        title = "Cluster Visualization",
-        width = 12
-      )
-    ),
-    title = "Clustering Results",
-    width = 12
-  )
-})
-
-output$parameters <- renderUI({
-  shinydashboard::box(
-    column(uiOutput("k"),
-           uiOutput("xdim"),
-           uiOutput("ydim"), width = 6),
-    column(
-      uiOutput("useFeatures"),
-      uiOutput("featuresOut"),
-      uiOutput("dimReduction"),
-      width = 6
-    ),
-    title = "Choose Clustering Parameters",
-    width = 10
-  )
 })
