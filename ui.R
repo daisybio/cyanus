10770576968818cd46f2e0488ce4ccf352e719c2
lisiarend <lisiarend@me.com>
2021-03-24 16:03:11 +0100

    more info while loading

--- ui.R
+++ ui.R
@@ -48,10 +48,21 @@ body <-
         disabled = TRUE
       )
     )),
-    div(
-      id = "loading",
-      "This app is currently loading. Please be patient. This may take a minute."
-    ),
+     div(
+       id = "loading",
+       fluidRow(box(
+         div ("CyTOF is an uprising method for discovering biomarkers of the immune system. 
+                              Its advantage over Flow Cytometry is that it labels the antibodies with metal 
+                              isotopes instead of fluorophores, allowing it to analyse more markers in a single 
+                              run while needing fewer cells."),
+         div ("Consequently, <b>CyTOF</b> experiments are becoming a powerful tool to find 
+                              immune marker expression differences between different conditions. In order 
+                              to facilitate the analysis of CyTOF data for biologists and physicians, 
+                              a clear, understandable and user-friendly pipeline is needed."),
+         title = h1("Welcome to the CyTOF Pipeline"),
+         width = 12
+       )),
+     ),
     tabItems(
       welcomeBody,
       startBody(),
