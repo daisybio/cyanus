ebf393ea6571ab0175c651b2b1e266b6fa4fd9c3
lisiarend <lisiarend@me.com>
2021-04-27 08:19:27 +0200

    negative binomial functions cleaned

--- DEComparison/zero_inflated_beta.R
+++ DEComparison/zero_inflated_beta.R
@@ -8,42 +8,14 @@ source("functions/de_functions.R")
 source("functions/cytoGLMM_functions.R")
 source("functions/ZIBseq_functions.R")
 
+# cytoGLMM simulator (5 of 20 markers significant)
+sce_cytoGLMM <- simulateSCE(n_true = 5, n_markers = 10)
 
-data(PBMC_panel, PBMC_md, PBMC_fs)
-sce_pbmc<- prepData(PBMC_fs, PBMC_panel, PBMC_md, transform=T)
+CATALYST::plotExprs(sce_cytoGLMM, color_by = "condition")
 
-sce_pbmc <- clusterSCE(sce_pbmc)
+CATALYST::plotExprHeatmap(sce_cytoGLMM, features = NULL, scale="never")
 
-rowData(sce_pbmc)$marker_class <- rowData(sce_pbmc)$marker_class[rowData(sce_pbmc)$marker_class == "none"] <- "state"
-
-
-# check diffcyt LMM no weights
-markers_to_test <- getMarkersToTest(sce_pbmc,"LMM","all")
-LMM_res_no_weights <- diffcyt_method(d_input = sce_pbmc,
-                                     formula = createFormula(ei(sce_pbmc), cols_fixed = c("condition")),
-                                     contrast = createContrast(c(0,1)),
-                                     analysis_type = "DS",
-                                     method_DS = "diffcyt-DS-LMM",
-                                     clustering_to_use = "all",
-                                     use_weights = FALSE,
-                                     markers_to_test = markers_to_test)
-
-results_LMM <- as.data.table(rowData(LMM_res_no_weights$res))$p_adj
-names(results_LMM) <- as.data.table(rowData(LMM_res_no_weights$res))$marker_id
-
-# ZIBSeq
-result <- zibSeq(sce = sce_pbmc, condition = "condition")
-padj <- p.adjust(result$pvalues, method="BH")
-names(padj) <- colnames(exprs)
+result <- zibSeq(sce = sce_cytoGLMM, condition = "condition", random_effect = "patient_id")
 
 
-# simulated data set
-sce_cytoGLMM <- simulateSCE()
-
-result <- zibSeq(sce = sce_cytoGLMM, condition = "condition")
-padj <- p.adjust(result$pvalues, method="BH")
-names(padj) <- colnames(exprs)
-
-results_cytoGLMM <- runCytoGLMM(sce_cytoGLMM, "condition", "patient_id")
-
-
+result_no_random <- zibSeq(sce = sce_cytoGLMM, condition = "condition")
--- functions/ZIBseq_functions.R
+++ functions/ZIBseq_functions.R
@@ -1,16 +1,39 @@
-zibSeq <- function (sce, condition, alpha = 0.05)
+zibSeq <- function (sce, condition, random_effect=NULL, alpha = 0.05)
 {
   X = t(as.data.frame(assays(sce)$exprs))
   Y = colData(sce)[[condition]]
+  
+  if (!is.null(random_effect)){
+    R = colData(sce)[[random_effect]]
+    message("Fitting a zero-inflated beta mixed model with random effects...")
+  } else {
+    message("Fitting a zero-inflated beta model...")
+  }
   P = dim(X)[2]
   beta = matrix(data = NA, P, 2)
   for (i in 1:P) {
+    message(paste("Fitting marker", colnames(X)[i]))
+    
     x.prop <- X[, i]
     x.prop[x.prop < 0] <- 0
     x.prop <- (x.prop - min(x.prop))/(max(x.prop)-min(x.prop))
     x.prop[which(x.prop==1)] <- x.prop[which(x.prop==1)] - 2.225074e-10
-    bereg = gamlss::gamlss(x.prop ~ Y, family = BEZI(), 
-                   trace = FALSE, control = gamlss.control(n.cyc = 100))
+    data <- data.table(
+      exprs = x.prop,
+      condition = Y
+    )
+    
+    if (!is.null(random_effect)){ 
+      # with random effect
+      data$random = R
+      bereg = gamlss::gamlss(exprs ~ condition + re(random=(~1|random)), family = BEZI(), 
+                             trace = FALSE, control = gamlss.control(n.cyc = 100), data = data)
+    } else {
+      # without random effect
+      bereg = gamlss::gamlss(exprs ~ condition, family = BEZI(), 
+                             trace = FALSE, control = gamlss.control(n.cyc = 100), data = data)
+    }
+  
     out = summary(bereg)
     beta[i, ] = out[2, c(1, 4)]
   }
