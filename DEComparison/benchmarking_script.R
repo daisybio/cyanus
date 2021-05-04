1a92b6b0aa395118b0d4fcd39a19680fb41d222f
JudithBernett <bernett@in.tum.de>
2021-05-04 15:16:44 +0200

    timing run DS

--- DEComparison/benchmarking_script.R
+++ DEComparison/benchmarking_script.R
@@ -1,23 +1,32 @@
-sce <- readRDS("/nfs/home/students/l.arend/data/covid_spiked/sce_spiked_clustered_ds_25.rds")
-
-source("functions/de_functions.R")
-source("functions/sceEMD.R")
-source("functions/cytoGLMM_functions.R")
-source("functions/ZIBseq_functions.R")
-source("functions/venn_functions.R")
+sce <- readRDS("/nfs/home/students/l.arend/data/covid_spiked/sce_spiked_clustered_ds_full.rds")
+sapply(list.files("./functions", full.names = TRUE), source)
 library(BiocParallel)
 param <- MulticoreParam(workers = 18, progressbar = T)
 register(param)
 results <- runDS(sce, 
-                 ds_methods = c("diffcyt-DS-limma","diffcyt-DS-LMM", "sceEMD", "ZIBseq"), 
-                 clustering_to_use = "all", contrast_vars = "base_spike", 
-                 design_matrix_vars = c("patient_id", "base_spike"), fixed_effects = "base_spike", 
-                 random_effects = "patient_id", markers_to_test = c("type", "state"), 
-                 sceEMD_condition = "base_spike", binSize = 0, nperm = 500, parallel=T, time_methods = T)
+                 clustering_to_use = "all",
+                 contrast_vars = "base_spike", 
+                 markers_to_test = "state", 
+                 ds_methods = c("diffcyt-DS-limma",
+                                "diffcyt-DS-LMM",
+                                "sceEMD",
+                                "ZIBseq",
+                                "hurdleBeta",
+                                "CytoGLMM"), 
+                 design_matrix_vars = c("patient_id", "base_spike"), 
+                 fixed_effects = "base_spike", 
+                 random_effects = "patient_id", 
+                 parallel = T,
+                 sceEMD_nperm = 500,
+                 sceEMD_binsize = 0, 
+                 time_methods = T)
 
 res <- results[["results"]]
 times <- results[["times"]]
-createVennDiagram(res, DS=T, 0.05)
+#only possible for max. 4 sets
+#createVennDiagram(res, DS=T, 0.05)
+dt <- data.table::rbindlist(sapply(res, data.table::as.data.table), fill = T, idcol="method")
+dt[p_adj <= 0.05]
 library(ggplot2)
 dt <- data.table::data.table(
   method = character(),
