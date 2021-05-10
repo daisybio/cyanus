5f0ebdfbbbebd71c8d12ef3887a9b9c408a57be2
JudithBernett <bernett@in.tum.de>
2021-05-10 11:17:49 +0200

    new renv, trying regularization

---
+++ functions/regularized_functions.R
@@ -0,0 +1,22 @@
+library(glmnet)
+sce <- readRDS("/nfs/home/students/l.arend/data/covid_spiked/downsampled_files/sce_spiked_clustered_full_ds_15000.rds")
+
+regularizedSCE <- function(sce, 
+                           condition, 
+                           features = SummarizedExperiment::rowData(sce)$marker_name,
+                           assay_to_use = "exprs",
+                           weighted = TRUE,
+                           alpha=0){
+  match.arg(assay_to_use, SummarizedExperiment::assayNames(sce))
+  match.arg(condition, names(SummarizedExperiment::colData(sce)))
+  features <-
+    match.arg(features,
+              SummarizedExperiment::rowData(sce)$marker_name,
+              several.ok = TRUE)
+  X <- t(SummarizedExperiment::assay(sce, assay_to_use))
+  X <- X[, features]
+  X <- (abs(X) + X) / 2
+  y <- sce[[condition]]
+  fit <- glmnet::cv.glmnet(X, y, family = "binomial", alpha=0)  
+  
+}
