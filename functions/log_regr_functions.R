70202f03f18744d056970bf2c790c8cd0126af4f
JudithBernett <bernett@in.tum.de>
2021-05-12 10:48:21 +0200

    fixed cytoGLMM parallel bug, made logistic regression function parallel

--- functions/log_regr_functions.R
+++ functions/log_regr_functions.R
@@ -4,42 +4,52 @@ logistic_regression <- function(sce,
                                 condition,
                                 random_effect = NULL,
                                 features = SummarizedExperiment::rowData(sce)$marker_name,
-                                assay_to_use = "exprs"){
+                                assay_to_use = "exprs", 
+                                parallel = FALSE){
   
   # Controls
   match.arg(assay_to_use, SummarizedExperiment::assayNames(sce))
   match.arg(condition, names(SummarizedExperiment::colData(sce)))
   features <- match.arg(features, SummarizedExperiment:: rowData(sce)$marker_name, several.ok = TRUE)
   
-  # Random Effects
-  if (!is.null(random_effect)){
-    for (re in random_effect){
-      match.arg(re, names(SummarizedExperiment::colData(sce)))
-    }
-    R <- SummarizedExperiment::colData(sce)[random_effect]
-    message(paste("...including random effects:", toString(random_effect)))
-  }
+  #parallelization
+  stopifnot(is.logical(parallel))
+  
+  bppar <- BiocParallel::bpparam()
+  if (!parallel)
+    bppar <- BiocParallel::SerialParam(progressbar = TRUE)
   
   # Data Preparation
   X <- t(as.data.frame(SummarizedExperiment::assay(sce, assay_to_use)))
   data <- data.frame(X[, features], y = SummarizedExperiment::colData(sce)[[condition]])
   
   if(is.null(random_effect)){
-    p.values <- sapply(features, function(f){
+    p.values <- data.table::rbindlist(BiocParallel::bplapply(features, function(f){
       fit <- glm(as.formula(paste("y ~ ", f)), family=binomial, data)
-      return(coef(summary(fit))[2,4])
-    })
+      return(data.table::data.table(
+        marker_id = f,
+        p_val = coef(summary(fit))[2,4]
+      ))
+    }, BPPARAM = bppar))
   }else{
+    for (re in random_effect){
+      match.arg(re, names(SummarizedExperiment::colData(sce)))
+    }
+    R <- SummarizedExperiment::colData(sce)[random_effect]
+    message(paste("...including random effects:", toString(random_effect)))
     data <- cbind(data, R)
-    p.values <- sapply(features, function(f){
+    p.values <- data.table::rbindlist(BiocParallel::bplapply(features, function(f){
       rand_formula <- paste(paste0("( 1 | ", random_effect, ")"), 
                                           collapse = " + ")
       formula <- as.formula(paste("y ~ ", f, "+", rand_formula))
       fit <- lme4::glmer(formula = formula, family=binomial, data = data)
-      return(coef(summary(fit))[2,4])
-    })
+      return(data.table::data.table(
+        marker_id = f,
+        p_val = coef(summary(fit))[2,4]
+      ))
+    }, BPPARAM = bppar))
   }
-  return(data.frame(marker_id = names(p.values), p_val = p.values))
+  return(p.values)
 }
 
 
