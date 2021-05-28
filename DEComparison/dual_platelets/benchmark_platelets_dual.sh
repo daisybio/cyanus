530a35cb3d7d5593e00e3f436241b7751795cb5c
lisiarend <lisiarend@me.com>
2021-05-28 09:07:24 +0200

    changing benchmarking script and running dual platelets dataset

---
+++ DEComparison/dual_platelets/benchmark_platelets_dual.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name=cytofDE_dual_platelets
+#SBATCH --output=%x.%j.txt
+#SBATCH --mem-per-cpu=70G
+
+Rscript ../benchmarking_script.R /nfs/home/students/l.arend/data/platelets_dual /nfs/home/students/l.arend/cytof/DEComparison/dual_platelets platelets patient_id TRUE FALSE
+
