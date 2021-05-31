1676473ce47143fc4eda6296eac0d65502a6e1d5
Quirin Manz <qu.manz@tum.de>
2021-05-31 12:40:04 +0200

    add pbmc to git

---
+++ DEComparison/pbmc_benchmarking/benchmark_pbmc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name=pbmc_benchmarking_cytof
+#SBATCH --output=%x.%j.txt
+#SBATCH --mem=20G
+
+Rscript ../benchmarking_script.R /nfs/home/students/jbernett/cytof/cytof/data/cytof_workflow_SCE.rds /nfs/home/students/jbernett/cytof/cytof/DEComparison/pbmc_benchmarking condition patient_id TRUE FALSE merging1
