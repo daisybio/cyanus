c5a4c7f5d0beaab0204bb459c62d118237e5794e
lisiarend <lisiarend@me.com>
2021-05-31 15:28:42 +0200

    evaluation dual plot updated

--- DEComparison/dual_platelets/evaluation.R
+++ DEComparison/dual_platelets/evaluation.R
@@ -21,14 +21,13 @@ times[, dataset := basename(dataset)]
 # plot times
 library(ggplot2)
 times$usage <- c("median", "median", "whole expression", "whole expression", "whole expression", "whole expression", "whole expression", "whole expression","median", "median")
-
-ggplot(times, aes(x = method, y = elapsed, fill = method)) +
+ggplot(times, aes(x = reorder(method,elapsed), y = elapsed, fill = method)) +
   geom_bar(size = 3, stat = "identity") +
-  theme_bw() + theme(text = element_text(size = 18), axis.text.x = element_text(angle = 30))
+  theme_bw() + theme(text = element_text(size = 18), axis.text.x = element_text(angle = 45, hjust=1)) + xlab(label="method") + ggtitle("Dual Platelets with Random Effect")
 
-ggplot(times, aes(x = method, y = elapsed, fill = usage)) +
-  geom_bar(size = 3, stat = "identity") +
-  theme_bw() + theme(text = element_text(size = 18), axis.text.x = element_text(angle = 30))
+#ggplot(times, aes(x = method, y = elapsed, fill = usage)) +
+#  geom_bar(size = 3, stat = "identity") +
+#  theme_bw() + theme(text = element_text(size = 18), axis.text.x = element_text(angle = 30))
 
 # plot results
 tmp <- data.frame(method = results$method, marker_id = results$marker_id, p_adj = results$p_adj)
@@ -40,6 +39,6 @@ tmp$class <- tmp$marker_id %in% c("CD62P", "CD63", "CD154", "CD107a")
 tmp$class[tmp$class == TRUE] <- "state"
 tmp$class[tmp$class == FALSE] <- "type"
 
-ggplot(tmp, aes(marker_id, method, fill=significant)) + geom_tile() + ggtitle("Dual Platelets") + xlab(label="marker") + facet_wrap(~class, scales = "free_x")
+ggplot(tmp, aes(marker_id, method, fill=significant)) + geom_tile(color="white", size=1) + ggtitle("Dual Platelets") + xlab(label="marker") + facet_wrap(~class, scales = "free_x") + theme(text = element_text(size = 16),  axis.text.x = element_text(angle = 45, hjust=1))
 
 
