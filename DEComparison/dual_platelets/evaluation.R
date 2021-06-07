eb0929eb16deb8a17a583a98353d5030780678c9
lisiarend <lisiarend@me.com>
2021-06-07 17:58:27 +0200

    evaluation dual platelets

--- DEComparison/dual_platelets/evaluation.R
+++ DEComparison/dual_platelets/evaluation.R
@@ -1,6 +1,7 @@
 # DUAL PLATELETS EVALUATION
 
 path <- "DEComparison/dual_platelets"
+path <- "/nfs/home/students/jbernett/cytof/cytof/DEComparison/dual_platelets"
 
 result_rds <-
   list.files(path = path,
@@ -18,12 +19,17 @@ times <-
 times[, dataset := basename(dataset)]
 
 
+times_re$random_effect <- "with random_effect"
+times$random_effect <- "without random_effect"
+
+tmp <- rbind(times, times_re)
+
 # plot times
 library(ggplot2)
 times$usage <- c("median", "median", "whole expression", "whole expression", "whole expression", "whole expression", "whole expression", "whole expression","median", "median")
-ggplot(times, aes(x = reorder(method,elapsed), y = elapsed, fill = method)) +
-  geom_bar(size = 3, stat = "identity") +
-  theme_bw() + theme(text = element_text(size = 18), axis.text.x = element_text(angle = 45, hjust=1)) + xlab(label="method") + ggtitle("Dual Platelets with Random Effect")
+ggplot(tmp, aes(x = reorder(method,elapsed), y = elapsed, fill = random_effect))+ #+ facet_wrap(~random_effect) + 
+  geom_bar(size = 3, stat = "identity", position="dodge") +
+  theme_bw() + theme(text = element_text(size = 18), axis.text.x = element_text(angle = 45, hjust=1)) + xlab(label="method") + ggtitle("Dual Platelets")
 
 #ggplot(times, aes(x = method, y = elapsed, fill = usage)) +
 #  geom_bar(size = 3, stat = "identity") +
@@ -39,6 +45,6 @@ tmp$class <- tmp$marker_id %in% c("CD62P", "CD63", "CD154", "CD107a")
 tmp$class[tmp$class == TRUE] <- "state"
 tmp$class[tmp$class == FALSE] <- "type"
 
-ggplot(tmp, aes(marker_id, method, fill=significant)) + geom_tile(color="white", size=1) + ggtitle("Dual Platelets") + xlab(label="marker") + facet_wrap(~class, scales = "free_x") + theme(text = element_text(size = 16),  axis.text.x = element_text(angle = 45, hjust=1))
+ggplot(tmp, aes(marker_id, method, fill=significant)) + geom_tile(color="white", size=1) + ggtitle("Dual Platelets without random effect") + xlab(label="marker") + facet_wrap(~class, scales = "free_x") + theme(text = element_text(size = 16),  axis.text.x = element_text(angle = 45, hjust=1))
 
 
