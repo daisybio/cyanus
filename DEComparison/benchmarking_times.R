91d4f5400a93965ffab6e5c06256b25ee2fa9fdf
lisiarend <lisiarend@me.com>
2021-06-15 22:23:19 +0200

    evaluation scripts changed & benchmarking_times added

---
+++ DEComparison/benchmarking_times.R
@@ -0,0 +1,92 @@
+# BECHNMARKING TIMES
+
+getTimes <- function(path){
+  result_rds <-
+    list.files(path = path,
+               pattern = "\\.rds$",
+               full.names = T)
+  times <-
+    data.table::rbindlist(sapply(result_rds, function(rds)
+      readRDS(rds)[["times"]], simplify = FALSE),
+      idcol = "dataset")
+  times[, dataset := basename(dataset)]
+  return(times[,c("dataset", "method", "elapsed")])
+}
+
+# Simulated CytoGLMM
+nr_of_samples<- 22
+cytoglmm_times <- getTimes("DEComparison/simulatedCytoGLMM")
+cytoglmm_times$nr_of_cells <- sapply(strsplit(cytoglmm_times$dataset,'_'), "[", 3)
+cytoglmm_times$nr_of_cells <- as.numeric(cytoglmm_times$nr_of_cells)
+cytoglmm_times$nr_of_cells <- cytoglmm_times$nr_of_cells * nr_of_samples
+cytoglmm_times$dataset <- "simulated_CytoGLMM"
+cytoglmm_times$random_effect <- "yes"
+
+# Dual Platelets
+sce_dual <- readRDS("~/data/platelets_dual/sce_dual.rds")
+nr_of_cells <- sum(ei(sce_dual)$n_cells)
+
+path_no_r <- "/nfs/home/students/l.arend/cytof/DEComparison/dual_platelets/sce_dual_res_timed_no_random.rds"
+path_with_r <- "/nfs/home/students/l.arend/cytof/DEComparison/dual_platelets/sce_dual_res_timed.rds"   #with random effects
+
+dual_random_times <- readRDS(path_with_r)[["times"]]
+dual_random_times <- dual_random_times[,c("method", "elapsed")]
+dual_random_times$dataset <- "dual_platelets_random"
+dual_random_times$random_effect <- "yes"
+dual_random_times$nr_of_cells <- nr_of_cells
+
+
+dual_no_times <- readRDS(path_no_r)[["times"]]
+dual_no_times <- dual_no_times[,c("method", "elapsed")]
+dual_no_times$dataset <- "dual_platelets"
+dual_no_times$random_effect <- "no"
+dual_no_times$nr_of_cells <- nr_of_cells
+
+
+# PBMC Data
+sce_pbmc <- readRDS("~/data/pbmc_all/bigPBMC_SCE.rds")
+nr_of_cells <- sum(ei(sce_pbmc)$n_cells)
+
+path_pbmc <- "~/cytof/DEComparison/pbmc_benchmarking/cytof_workflow_SCE_res_timed.rds"
+pbmc_times <- readRDS(path_pbmc)[["times"]]
+pbmc_times <- pbmc_times[,c("method", "elapsed")]
+pbmc_times$dataset <- "pbmc"
+pbmc_times$random_effect <- "yes"
+pbmc_times$nr_of_cells <- nr_of_cells
+
+
+# Simulated Covid-Spike
+nr_of_samples<- 22
+covid_times <- getTimes("DEComparison/downsampled_covid_spike")
+covid_times$nr_of_cells <- sapply(strsplit(covid_times$dataset,'_'), "[", 6)
+covid_times$nr_of_cells <- as.numeric(covid_times$nr_of_cells)
+covid_times$nr_of_cells <- covid_times$nr_of_cells * nr_of_samples
+covid_times$dataset <- "covid_spiked"
+covid_times$random_effect <- "yes"
+
+
+# Plot
+safe_colorblind_palette <- c("#88CCEE", "#CC6677", "#DDCC77", "#117733", "#332288", "#AA4499", 
+                             "#44AA99", "#999933", "#882255", "#661100", "#6699CC")
+
+times <- rbind(dual_random_times, dual_no_times, cytoglmm_times, pbmc_times, covid_times)
+times$size <- times$nr_of_cells<1000000
+times$size[times$size==TRUE] <- "Small Dataset"
+times$size[times$size==FALSE] <- "Big Dataset"
+times$size <- factor(times$size, levels=c("Small Dataset", "Big Dataset"))
+times$elapsed <- times$elapsed / 60
+ggplot(times,
+               aes(
+                 x = nr_of_cells,
+                 y = elapsed,
+                 color = method,
+                 shape = dataset,
+  )) + 
+  scale_shape_manual(values=1:times[, data.table::uniqueN(method)]) +
+  facet_wrap(~size, scales="free") +
+  geom_point(size = 4, stroke=1.5) + xlab("number of cells") + ylab("elapsed (in min.)") +
+  theme_bw() + theme(text = element_text(size = 16), axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_color_manual(values = safe_colorblind_palette)
+
+
+        
--- DEComparison/simulatedCytoGLMM/evaluation.R
+++ DEComparison/simulatedCytoGLMM/evaluation.R
@@ -8,3 +8,65 @@ plot_sens_vs_pre(stats_table)
 plot_sens_vs_spec(stats_table)
 plot_f1_vs_elapsed(stats_table)
 plotHeatmaps('simulatedCytoGLMM')
+
+
+# Plot Heatmap for Paper
+path <-  "DEComparison/simulatedCytoGLMM"
+trues <- c("m01", "m02", "m03", "m04", "m05")
+keep <- 3
+
+result_rds <-
+  list.files(path = path,
+             pattern = "\\.rds$",
+             full.names = T)
+results <-
+  data.table::rbindlist(sapply(result_rds, function(rds)
+    readRDS(rds)[["results"]], simplify = FALSE),
+    idcol = "dataset")
+results[, dataset := basename(dataset)]
+results[, nr_of_cells := tstrsplit(dataset, "_", keep=keep)]
+tmp <- data.frame(method = results$method, marker_id = results$marker_id, p_adj = results$p_adj, nr_of_cells = results$nr_of_cells)
+tmp$significant <- results$p_adj < 0.05
+tmp$nr_of_cells <- results$nr_of_cells
+tmp$p_adj <- NULL
+tmp <- as.data.table(tmp)
+tmp$significant <- as.factor(tmp$significant)
+tmp$class <- tmp$marker_id %in% trues
+tmp$class[tmp$class == TRUE] <- "differentially expressed"
+tmp$class[tmp$class == FALSE] <- "not differentially expressed"
+
+
+eff <-
+  data.table::rbindlist(sapply(result_rds, function(rds)
+    readRDS(rds)[["eff"]], simplify = FALSE),
+    idcol = "dataset")
+eff[, dataset := basename(dataset)]
+eff$nr_of_cells <- sapply(strsplit(eff$dataset,'_'), "[", 3)
+eff$marker_id <- sapply(strsplit(eff$group1,'::'), "[", 1)
+
+
+
+colorBlindBlack8  <- c("#000000", "#E69F00", "#56B4E9", "#009E73", 
+                       "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+# for paper only 200000 cells
+#tmp <- tmp[tmp$nr_of_cells=="200000",]
+marker_classes <- tmp[,c("marker_id", "class")]
+marker_classes <- unique(marker_classes)
+#eff <- eff[eff$nr_of_cells=="200000",]
+eff <- eff[,c("overall_group", "magnitude", "marker_id", "nr_of_cells")]
+eff <- merge(eff,marker_classes, by="marker_id")
+
+ggplot(tmp, aes(marker_id, method)) + 
+  geom_tile(aes(fill=significant), color="white", size=1) + 
+  ggtitle("") + 
+  xlab(label="marker") + 
+  #facet_wrap(~class, scales = "free_x") + 
+  facet_grid(nr_of_cells~class, scales = "free_x") +
+  theme(text = element_text(size = 16),  axis.text.x = element_text(angle = 45, hjust=1))+
+  scale_fill_manual(values = colorBlindBlack8[c(7,3)], na.value="transparent") + 
+  ggside::geom_xsidetile(data=eff, aes(y=overall_group, xfill=magnitude), color="white", size=0.2) + 
+  ggside::scale_xfill_manual(values=colorBlindBlack8[c(8,5,2,6)], name='effect size\nmagnitude', na.value="transparent", drop=FALSE)
+
+
+
