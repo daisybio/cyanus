74b63e79a682d54a5e912b284bfdb045775b26c7
JudithBernett <bernett@in.tum.de>
2024-01-25 09:48:27 +0100

    moving DataGeneration and DEComparison to new location

--- DEComparison/downsampled_covid_spike/benchmark_downsampled_covid_spike.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --job-name=covid_spike_downsample
-#SBATCH --output=%x.%j.txt
-#SBATCH --mem=50G
-
-Rscript ../benchmarking_script.R DataGeneration/covid_spiked DEComparison/downsampled_covid_spike base_spike patient_id TRUE FALSE
